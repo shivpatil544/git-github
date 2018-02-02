a694882fced44a12f517d54ceb0e5676ca5d489e
shivpatil544 <shivanand_patil@thbs.com>
2018-02-02 12:19:48 +0530

    java-files

---
+++ Arithematic.java
@@ -0,0 +1,14 @@
+class Arithematic {
+	public static void main(String args[]){
+		int a = 1;
+		int b =10;
+		int c=5;
+
+		if(a==1){
+			System.out.println(b+c);
+		}
+		else{
+			System.out.println(b-c);
+		}
+	}
+}
